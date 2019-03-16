de4565a1acd0263e3f22877c914c0b19881cda9f
František Maštera <xmaste02@stud.fit.vutbr.cz>
2019-03-16 12:05:11 +0100

    [#7] Implement trigonometric functions

--- src/math_lib.py
+++ src/math_lib.py
@@ -8,7 +8,7 @@
 #
 # Project: fit-ivs-2
 # Date created: 2019-03-05
-# Last modified: 2019-03-15
+# Last modified: 2019-03-16
 #
 
 PI = 3.141_592_653_589_793
@@ -122,26 +122,154 @@ def nrt(x, n):
         raise ValueError('Can\'t get 0th root of any number.')
 
 
-# TODO: rad reduction to <-2*PI; 2*PI> interval
+##
+# @brief Reduce number to (-2*PI, 2*PI) range by subtracting/adding 2*PI.
+# @param x Number to be reduced.
+# @return x within (-2*PI, 2*PI) range with the same sine/cosine value as original x.
+# Subtracts/adds 2*PI from/to the x value until it's within (-2*PI, 2*PI) range. Due to limited
+# PI accuracy, this means the x gets less accurate the higher/lower it is in the beggining.
+# This is neccessary because sin(x) and cos(x) with x values set too high or too low
+# cause OverflowError.
+#
 def reduce_rad(x):
-    return 0
+    while x > 2*PI:
+        x -= 2*PI
+    while x < -2*PI:
+        x += 2*PI
+    return x
 
 
-# TODO: sin of x
+##
+# @brief Sine of a number using Taylor series.
+# @param n Number of which sine is calculated in radians.
+# @return Sine of given number.
+# @see https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
+# Values outisde (-2*PI, 2*PI) range will be increased/decreased by 2*PI until they will
+# get to that range. This shouldn't affect the result unless the x is ludicrously small/large.
+#
 def sin(x):
-    return 0
+    x = reduce_rad(x)
+
+    # Store values between iterations to prevent calculating the same values over and over again.
+    sinx = 0
+    odd = 1
+    facx = 1
+    powx = x
+
+    n = 0
+    while True:
+
+        # Calculate sine using Taylor series.
+        prev_sinx = sinx
+        sinx += odd * powx / facx
+        diff = sinx - prev_sinx
+
+        # If difference between result of this and last iteration is negligible, stop iterating.
+        if -acc < diff < acc:
+            break
+
+        # Increment values used in formula for next iteration.
+        n += 1
+        odd *= -1
+        facx *= (2 * n) * (2 * n + 1)
+        powx *= x * x
+
+    return sinx
 
 
-# TODO: arcsin of x
+##
+# @brief Arcsine of a number using Taylor series.
+# @param n Number of which arcsine is calculated.
+# @return Arcsine of given number in radians.
+# @exception ValueError if x is outide the (-1, 1) range.
+# @see https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
+# Values close to minimal/maximal allowed values (from +-0.999) will return set value
+# of PI/2. Calculation of precise result for these values takes long time to process.
+#
 def asin(x):
-    return 0
+    if x < -1 or x > 1:
+        raise ValueError('Arcus sine isn\'t defined outside the <-1, 1> range.')
+    if x >= 0.999:
+        return PI / 2
+    if x <= -0.999:
+        return -PI / 2
+
+    # Store values between iterations to prevent calculating the same values over and over again.
+    asinx = 0
+    fac_even = 1
+    pow_facx = 1
+    pow_even = x
+    pow_4 = 1
+
+    n = 0
+    while True:
+
+        # Calculate arcsine using Taylor series.
+        prev_sinx = asinx
+        asinx += (fac_even / (pow_4*pow_facx*((n << 1) + 1))) * pow_even
+        diff = asinx - prev_sinx
+
+        # If difference between result of this and last iteration is negligible, stop iterating.
+        if -acc < diff < acc:
+            break
+
+        # Increment values used in formula for next iteration.
+        n += 1
+        fac_even *= (((n << 1) - 1) * (n << 1))
+        pow_facx *= n * n
+        pow_even *= x * x
+        pow_4 <<= 2
+
+    return asinx
 
 
-# TODO: cos of x
+##
+# @brief Cosine of a number using Taylor series.
+# @param n Number of which sine is calculated in radians.
+# @return Cosine of given number.
+# @see https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
+# Values outisde (-2*PI, 2*PI) range will be increased/decreased by 2*PI until they will
+# get to that range. This shouldn't affect the result unless the x is ludicrously small/large.
+#
 def cos(x):
-    return 0
+    x = reduce_rad(x)
+
+    # Store values between iterations to prevent calculating the same values over and over again.
+    cosx = 0
+    odd = 1
+    facx = 1
+    powx = 1
+
+    n = 0
+    while True:
+
+        # Calculate cosine using Taylor series.
+        prev_sinx = cosx
+        cosx += odd * powx / facx
+        diff = cosx - prev_sinx
+
+        # If difference between result of this and last iteration is negligible, stop iterating.
+        if -acc < diff < acc:
+            break
+
+        # Increment values used in formula for next iteration.
+        n += 1
+        odd *= -1
+        facx *= (2*n - 1) * (2*n)
+        powx *= x * x
+
+    return cosx
 
 
-# TODO: arccos of x
+##
+# @brief Arccosine of a number using Taylor series.
+# @param n Number of which arccosine is calculated.
+# @return Arccosine of given number in radians.
+# @exception ValueError if x is outide the (-1, 1) range.
+# @see https://en.wikipedia.org/wiki/Taylor_series#Trigonometric_functions
+# Values close to minimal/maximal allowed values (from +-0.999) will return set values
+# of 0 and PI. Calculation of precise result for these values takes long time to process.
+#
 def acos(x):
-    return 0
+    return PI/2 - asin(x)
+
